36817dd9f9c6bbcf4c34b4472a854d3b141b783e
Roberto Cunha <rcunha@live.com>
2024-11-29 19:22:51 -0300

    feat: configurando tsc

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { server } from "./server/Server"
 
-server.listen(3333, 
-    () =>  console.log('⚡️ Aplicação inicializada na porta: 3333.')
+server.listen(process.env.PORT || 3333, 
+    () =>  console.log(`⚡️ Aplicação inicializada na porta: ${process.env.PORT }.`)
 )   
 
