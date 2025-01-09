88c5a2dbeb65d15c56cbf7523c926794761a3f6f
Roberto Cunha <rcunha@live.com>
2025-01-09 19:02:44 -0300

    fix: ajuste no retorno da getByIdPessoaUseCase

--- src/server/useCases/pessoas/GetById.ts
+++ src/server/useCases/pessoas/GetById.ts
@@ -1,13 +1,15 @@
 import { PessoasProvider } from "../../dataBase/providers/pessoas";
 import DataNotFoundException from "../../../core/exceptions/DataNotFoundException";
+import { IPessoa } from "../../dataBase/models";
 
 export class GetByIdPessoasUseCase {
-  async getById(id: number): Promise<void> {
+  async getById(id: number): Promise<IPessoa> {
     const result = await PessoasProvider.getById(id);
 
     if (!result) {
       throw new DataNotFoundException("Registro não encontrado!");
     }
+    return result
   }
 }
 
